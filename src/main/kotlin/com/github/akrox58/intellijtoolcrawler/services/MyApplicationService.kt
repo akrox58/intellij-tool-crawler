9383cf85b2c911a6b5bb3f4f5095e2f32412050e
GitHub Action <action@github.com>
2022-08-01 06:02:53 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/akrox58/intellijtoolcrawler/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.akrox58.intellijtoolcrawler.services
+
+import com.github.akrox58.intellijtoolcrawler.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
